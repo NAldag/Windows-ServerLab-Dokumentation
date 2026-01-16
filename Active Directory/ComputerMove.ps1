2b0d543aac1e402ef08281d51231e51dc0be46da
Nico Aldag <126572647+NAldag@users.noreply.github.com>
2026-01-16 15:42:38 +0100

    Update ComputerMove.ps1

--- Active Directory/ComputerMove.ps1
+++ Active Directory/ComputerMove.ps1
@@ -1,3 +1,3 @@
 # Beispiel: CLIENT01 nach OU Clients verschieben
 Move-ADObject -Identity "CN=CLIENT01,CN=Computers,DC=HomeLab,DC=local" `
-  -TargetPath "OU=Clients,OU=Computers,DC=HomeLab,DC=local"
+  -TargetPath "OU=Clients,OU=LabComputers,DC=HomeLab,DC=local"
