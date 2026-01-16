c7905030d86f0471a81de6c075db2332f813856b
Nico Aldag <126572647+NAldag@users.noreply.github.com>
2026-01-16 13:23:06 +0100

    Update 01_OUs.ps1

--- Active Directory/01_OUs.ps1
+++ Active Directory/01_OUs.ps1
@@ -1,4 +1,11 @@
 # Domänenbasis
+
+# Test: OUS wurden erstellt, aber wirft exceptions beim Abfragen existierender OUs, beim versuchten Erstellen exisiterender OUs und beim Erstellen von Unter-OUs aufgrund zeitlicher Auflösung zwischen AD und Skriptlauf. Das Skript läuft schneller durch, als dass die Objekte in AD sichtbar sind.
+# Abfrage & Erstellen: Basis OUs mit Namen gleich eines basis Containers geändert. Create-Execption mit Try Catch auffangen.
+# Zeitauflösung(pragmatisch): Minimales Delay nach Parent Erstellung, nicht garantiert: Start-Sleep -Milliseconds 500
+# Zeitauflösung(langfristig): Hilfsfunktion Wait-ForOU. Aufwändiger, aber erstlauf Garantie.
+
+
 $DomainDN = "DC=HomeLab,DC=local"
 
 # Sicherstellen, dass AD-Modul geladen ist
@@ -9,10 +16,9 @@ if (-not (Get-Module -ListAvailable ActiveDirectory)) {
 Import-Module ActiveDirectory
 
 # 01_OUs.ps1
-# Test: 
 # Erstellt die OU-Struktur für das HomeLab
 
-$BaseOUs = @("Users","Computers","Groups","ServiceAccounts")
+$BaseOUs = @("LabUsers","LabComputers","Groups","ServiceAccounts")
 $UserOUs = @("IT","Guests")
 $GroupOUs = @("Security","Distribution")
 
@@ -29,6 +35,9 @@ foreach ($ou in $BaseOUs) {
     }
 }
 
+Start-Sleep -Milliseconds 500
+
+
 foreach ($ou in $UserOUs) {
     $dn = "OU=$ou,OU=Users,$DomainDN"
     $ExistingOUs = Get-ADOrganizationalUnit -Identity $dn -ErrorAction SilentlyContinue
@@ -39,6 +48,8 @@ foreach ($ou in $UserOUs) {
     }
 }
 
+Start-Sleep -Milliseconds 500
+
 foreach ($ou in $GroupOUs) {
     $dn = "OU=$ou,OU=Groups,$DomainDN"
     $ExistingOUs = Get-ADOrganizationalUnit -Identity $dn -ErrorAction SilentlyContinue
@@ -48,6 +59,8 @@ foreach ($ou in $GroupOUs) {
         Write-Output "OU Groups\$ou erstellt"
     }
 }
+
+Start-Sleep -Milliseconds 500
 
 # Computer-Unter-OU
 $clientsDN = "OU=Clients,OU=Computers,$DomainDN"
